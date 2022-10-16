361703299728f03635da370c92508869670f111c
Damian Ho <damian_ho_xu_yang@yahoo.com>
2022-10-16 08:15:28 -0700

    Add upload and list APIs

--- facialrec/app.py
+++ facialrec/app.py
@@ -1,17 +1,16 @@
 # OpenCV and Flask modules
+import os
 import time
 import numpy
 import cv2
+import pathlib
 from flask import Flask, Response, render_template, request
+from werkzeug.utils import secure_filename
 from PIL import Image
 
 # Facial Recognition
 import face_recognition
 from simple_facerec import SimpleFacerec
-# Encode faces from a folder
-sfr = SimpleFacerec()
-sfr.load_encoding_images("images/")
-
 
 # Flask App
 app = Flask(__name__)
@@ -19,6 +18,12 @@ app = Flask(__name__)
 cam = cv2.VideoCapture(0)
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+app.config['UPLOAD_FOLDER'] = "./static/images"
+
+# Encode faces from a folder
+sfr = SimpleFacerec()
+sfr.load_encoding_images(app.config['UPLOAD_FOLDER'])
 
 # Function to encode facial recognition images on cv2 capture
 def facial_recognition():
@@ -35,24 +40,58 @@ def facial_recognition():
         _, img = cv2.imencode('.jpg', frame)
         yield (b'--frame\r\nContent-Type: imageviewer/jpeg\r\n\r\n' + img.tobytes() + b'\r\n')
 
-
 # Routing to Face Recognition
 # To change to capture images from video stream, change facial_recognition_image() function to facial_recognition()
 @app.route("/")
 def facial_rec():
     return Response(facial_recognition(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+# GET: POC to verify
+# POST: Endpoint
 @app.route("/verify", methods = ['GET', 'POST'])
 def verify():
     if request.method == 'GET':
-        return render_template('upload.html')
+        return render_template('verify.html')
     else:
         image = cv2.imdecode(numpy.fromstring(request.files['file'].read(), numpy.uint8), cv2.IMREAD_UNCHANGED)
         face_locations, face_names = sfr.detect_known_faces(image)
         name = request.form['name']
         for face_loc, face_name in zip(face_locations, face_names):
             if name == face_name:
-                return Response("Yes")
-                break
-        else:
-            return Response("No")
+                response = Response("MATCH\nExpected {}\nGot {}\n".format(name, (" ".join(face_names))))
+                response.headers["content-type"] = "text/plain"
+                return response
+        response = Response("FAIL\nExpected {}\nGot {}\n".format(name, (" ".join(face_names))))
+        response.headers["content-type"] = "text/plain"
+        return response
+
+def generate_filename(username, original_file_name):
+    return username + pathlib.Path(original_file_name).suffix
+
+@app.route('/upload', methods = ['GET', 'POST'])
+def upload():
+    if request.method == 'GET':
+        return render_template('upload.html')
+    else:
+        name = request.form['name']
+        file = request.files['file']
+        filename = generate_filename(name, file.filename)
+        pathname = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(pathname)
+        sfr.load_encoding_images("./static/images/")
+        response = Response("Uploaded to {}!\n".format(pathname))
+        response.headers["content-type"] = "text/plain"
+        return response
+
+@app.route("/list")
+def list():
+    pics = os.listdir(app.config['UPLOAD_FOLDER'])
+    pics = filter(lambda x: not x.startswith("."), pics)
+    abspath = app.config['UPLOAD_FOLDER']
+    pics = [
+        (   pathlib.Path(os.path.basename(pic)).stem,
+            os.path.join(abspath, pic)
+        )
+        for pic in pics
+    ]
+    return render_template('list.html', pics = pics)

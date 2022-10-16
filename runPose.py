f4cd816b2b8ea51f73250770c8d571add85ec39b
tjunjet <latyun142@gmail.com>
2022-10-16 08:03:56 -0700

    Done with the logic

--- runPose.py
+++ runPose.py
@@ -17,7 +17,6 @@ with mp_pose.Pose(
     image_height, image_width, _ = image.shape
     # Convert the BGR image to RGB before processing.
     results = pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-
     if not results.pose_landmarks:
       continue
     print(
@@ -61,8 +60,21 @@ with mp_pose.Pose(
     # pass by reference.
     image.flags.writeable = False
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = pose.process(image)
-
+    results = pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    # Do the shoulder press
+    if (results.pose_landmarks != None):
+      # Shoulders
+      right_shoulder = results.pose_landmarks.landmark[12]
+      left_shoulder = results.pose_landmarks.landmark[11]
+      right_hip = results.pose_landmarks.landmark[24]
+      left_hip = results.pose_landmarks.landmark[23]
+      # Coding the logic
+      if (right_shoulder.x > right_hip.x):
+        print("Right")
+      if (left_shoulder.x < left_hip.x):
+        print("Left")
+    else:
+      print("None")
     # Draw the pose annotation on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
@@ -71,7 +83,7 @@ with mp_pose.Pose(
         results.pose_landmarks,
         mp_pose.POSE_CONNECTIONS,
         landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-    # Flip the image horizontally for a selfie-view display.
+    # Flip the image horizontally for a selfie-view display
     cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
     if cv2.waitKey(5) & 0xFF == 27:
       break
